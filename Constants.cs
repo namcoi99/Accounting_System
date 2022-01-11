69108d058e61ad59459b7a30ed32483af621d9f9
namcoi99 <nam_1999_x@yahoo.com.vn>
2022-01-11 22:57:38 +0700

    test tree view menu

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accounting_System
+{
+    public class Constants
+    {
+        public const string FDM = "CatalogTable";
+    }
+}
---
+++ Helpers/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accounting_System.Helpers
+{
+    public class Utils
+    {
+        public static string GetPageName(string menuCommand)
+        {
+            switch(menuCommand) {
+                case "FDM":
+                    return Constants.FDM;
+                default:
+                    return "";
+            }
+        }
+    }
+}
